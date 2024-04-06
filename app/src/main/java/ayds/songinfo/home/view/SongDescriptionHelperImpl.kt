d5bf70c13b0e97a27e9743a82324deea96f8d356
Martina <martumoreno6@gmail.com>
2024-04-06 17:33:01 -0300

    Etapa 2 - cambios en SongDescriptionHelperImpl.kt y SpotifySong.kt

--- app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -3,6 +3,8 @@ package ayds.songinfo.home.view
 import ayds.songinfo.home.model.entities.Song.EmptySong
 import ayds.songinfo.home.model.entities.Song
 import ayds.songinfo.home.model.entities.Song.SpotifySong
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 interface SongDescriptionHelper {
     fun getSongDescriptionText(song: Song = EmptySong): String
@@ -18,8 +20,40 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
                 }\n" +
                         "Artist: ${song.artistName}\n" +
                         "Album: ${song.albumName}\n" +
-                        "Year: ${song.year}"
+                        "Year: ${getReleaseDate(song.releaseDate, song.release_date_precision)}"
             else -> "Song not found"
         }
     }
+
+private fun getReleaseDate(release: String, releasePrecision: String): String{
+    val format = when(releasePrecision) {
+        "day" -> changeFormatDay(release)
+        "month" -> changeFormatMonth(release)
+        else -> changeFormatYear(release)
+    }
+    return format
+}
+
+    private fun changeFormatDay(release: String): String {
+        val originalFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val dateDay = originalFormat.parse(release)
+        val newFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        return newFormat.format(dateDay)
+    }
+
+    private fun changeFormatMonth(release: String): String {
+        val originalFormat = SimpleDateFormat("yyyy-MM", Locale.getDefault())
+        val dateDay = originalFormat.parse(release)
+        val newFormat = SimpleDateFormat("MMMM, yyyy", Locale.getDefault())
+        return newFormat.format(dateDay)
+    }
+
+    private fun changeFormatYear(release: String): String {
+        return if (esB(release.split("-").first().toInt()))
+            "${release.split("-").first()} (a leap year)"
+        else
+            "${release.split("-").first()} (not a leap year)"
+    }
+
+    private fun esB(n: Int) = (n % 4 == 0) && (n % 100 != 0 || n % 400 == 0)
 }
